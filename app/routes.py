5e831c58817f9674de38d99f2396db38eb24a603
Defenderrus <postsergeyu@mail.ru>
2026-05-24 11:48:13 +0000

    Auto deploy

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ task_id = 0
 
 @router.get("/")
 def root():
-    return {"message": "Service is running! Auto deploy succesful!"}
+    return {"message": "Service is running, auto deploy succesful!"}
 
 
 @router.post("/tasks")
